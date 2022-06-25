21155c063ad0739ef9c1da33f1557910ee6b0f7f
acer <acer@192.168.0.101>
2022-06-25 12:29:26 +0530

    boolean

---
+++ SVKP_PROJECT/src/FloatDouble.java
@@ -0,0 +1,12 @@
+
+public class FloatDouble {
+	public static void main(String args[]) {
+		int value1 = 9/6;
+		float value2 = 10f/6f;
+		double value3 = 10d/6d;
+		System.out.println("value1="+value1);
+		System.out.println("value2="+value2);
+		System.out.println("value3="+value3);
+	}
+
+}
---
+++ SVKP_PROJECT/src/HelloDatatypes.java
@@ -0,0 +1,17 @@
+
+public class HelloDatatypes {
+
+	public static void main(String[] args) {
+		int max = 2147483647;
+		int min = -2147483647;
+		short shortMax=32767;
+		short shortMin = -32767;
+		long longMax=9223372036854775807L;
+		long longMin = -9223372036854775808L;
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println(byteMax);
+
+	}
+
+}
